ad80f55e336842c9125c4f88f00902ce30530acb
Marcelo Lanegra <marcelo3096@gmail.com>
2023-11-15 01:11:56 -0500

    feat: added Modal Component

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Modal from "@/components/Modal";
+import { useState, useEffect } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Modal title="Modal" description="Test" isOpen onChange={()=>{}}>
+        Testing
+      </Modal>
+    </>
+  )
+};
+
+export default ModalProvider;
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,23 +1,25 @@
-'use client'
+"use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Database } from "@/types_db"
+import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
   children: React.ReactNode;
 }
 
-const SupabaseProvider: React.FC<SupabaseProviderProps> = ({children}) => {
-  const [supabaseClient] = useState(() => createClientComponentClient<Database>());
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
 
   return (
-    <SessionContextProvider supabaseClient={{supabaseClient}}>
+    <SessionContextProvider supabaseClient={supabaseClient}>
       {children}
     </SessionContextProvider>
-  )
-}
+  );
+};
 
-export default SupabaseProvider
+export default SupabaseProvider;
